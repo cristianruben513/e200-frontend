08118e37f880c37b2809cd052f7e698c1b277926
Cristian Ruben <figueroac513@gmail.com>
2024-09-24 12:53:44 -0600

    add bg in auth layout

--- src/layouts/auth.tsx
+++ src/layouts/auth.tsx
@@ -7,7 +7,7 @@ export default function AuthLayout({
 }) {
   return (
     <div className='lg:h-[100dvh] h-[25dvh] flex-col items-center justify-center lg:grid  lg:grid-cols-5'>
-      <div className='w-full h-full flex-col lg:col-span-2 p-10 text-white bg-blue-500'>
+      <div className='w-full h-full flex-col lg:col-span-2 p-10 text-white bg-blue-800'>
         <div className='relative z-20 flex flex-row items-center gap-6 mb-3'>
           <p className='lg:text-4xl text-2xl font-black'>E200</p>
         </div>
@@ -17,10 +17,13 @@ export default function AuthLayout({
         className={cn(
           "lg:col-span-3",
           "lg:h-[100dvh] h-[65dvh]",
-          "flex items-center justify-center",
-          "bg-white"
+          "flex items-center justify-center"
         )}
       >
+        <div className='fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#e6e6e6_1px,transparent_1px),linear-gradient(to_bottom,#e6e6e6_1px,transparent_1px)] bg-[size:6rem_4rem]'>
+          <div className='absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#6fb1d8,transparent)]'></div>
+        </div>
+
         {children}
       </div>
     </div>
--- src/pages/auth/login/page.tsx
+++ src/pages/auth/login/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from "./form"
 export default function LoginPage() {
   return (
     <AuthLayout>
-      <div className='mx-auto flex w-full flex-col justify-center space-y-6 lg:p-8 md:border border-neutral-300 rounded-xl md:p-4 md:mx-16 max-w-lg'>
+      <div className='mx-auto flex w-full flex-col justify-center space-y-6 lg:p-8 md:border border-neutral-300 rounded-xl md:p-4 md:mx-16 max-w-lg bg-white'>
         <div className='flex flex-col space-y-2  md:p-0 px-6'>
           <h1 className='text-3xl md:text-4xl font-bold sm:my-5 my-5'>
             Iniciar sesion
